56e8f7a4a0313ddd17e2d5534dc36b6cfae6031e
Abdullah Ammar <halo@abdmmar.com>
2024-01-08 18:09:15 +0700

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import path from "path"
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
+  },
+})
