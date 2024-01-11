ebbc3828ff113bed519784ef4c5699937a26b5f5
Abdullah Ammar <halo@abdmmar.com>
2024-01-11 22:13:39 +0700

    test: add mock and init test

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,22 +1,9 @@
-import App from "./App";
+import App from "./app";
 import { render, screen, userEvent } from "./utils/test-utils";
 
-describe("Simple working test", () => {
-	it("the title is visible", () => {
+describe("Form", () => {
+	it("should render properly", () => {
 		render(<App />);
-		expect(screen.getByText(/Hello Vite \+ React!/i)).toBeInTheDocument();
-	});
-
-	it("should increment count on click", async () => {
-		render(<App />);
-		userEvent.click(screen.getByRole("button"));
-		expect(await screen.findByText(/count is: 1/i)).toBeInTheDocument();
-	});
-
-	it("uses flexbox in app header", async () => {
-		render(<App />);
-		const element = screen.getByRole("banner");
-		expect(element.className).toEqual("App-header");
-		expect(getComputedStyle(element).display).toEqual("flex");
+		expect(screen.getByLabelText(/title/i)).toBeInTheDocument();
 	});
 });
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,11 @@
 import "@testing-library/jest-dom/vitest";
+
+// Mock the ResizeObserver
+const ResizeObserverMock = vi.fn(() => ({
+	observe: vi.fn(),
+	unobserve: vi.fn(),
+	disconnect: vi.fn(),
+}));
+
+// Stub the global ResizeObserver
+vi.stubGlobal("ResizeObserver", ResizeObserverMock);
