3be146072ceaf7bf48d74b8dd6f334203faa0aad
Abdullah Ammar <halo@abdmmar.com>
2024-01-15 00:27:58 +0700

    test: add select value

--- src/form.test.tsx
+++ src/form.test.tsx
@@ -8,8 +8,8 @@ const collection = {
 		"Layaknya karya-karya bercorak Romantisisme, Raden Saleh mengungkapkan gejolak jiwanya yang terombang-ambing antara keinginan menghayati dunia imajinasi dan menyatakan dunia nyata. Perpaduan keduanya terwujud dalam ekspresi visual yang dramatis, emosional, sekaligus misterius. Meski demikian, para seniman romantisis sering juga berkarya berdasarkan pada kenyataan aktual. Dalam lukisan Kapal Dilanda Badai, dapat dilihat bagaimana Raden Saleh mengungkapkan perjuangan dramatis dua buah kapal dalam hempasan badai dahsyat di tengah lautan. Suasana mencekam diekspresikan lewat awan tebal yang gelap dan ombak-ombak tinggi yang menghancurkan salah satu kapal. Dari sudut atas, secercah sinar matahari tampak memantul ke gulungan ombak—menambah kesan dramatis.",
 	artist: "Raden Saleh",
 	creationDate: "1840-01-01",
-	classification: "Painting",
-	medium: "Oil on Canvas",
+	classification: { value: "painting", label: "Painting" },
+	medium: { value: "oil-on-canvas", label: "Oil on Canvas" },
 	photo: new File(["Kapal Karam Dilanda Badai"], "kapalKaramDilandaBadai.png", {
 		type: "image/png",
 	}),
@@ -49,8 +49,8 @@ describe("CollectionForm", () => {
 		await user.type(formElements.descriptionInput, collection.description);
 		await user.type(formElements.artistInput, collection.artist);
 		await user.type(formElements.creationDateInput, collection.creationDate);
-		await user.selectOptions(formElements.classificationInput, collection.classification);
-		await user.selectOptions(formElements.mediumInput, collection.medium);
+		await user.selectOptions(formElements.classificationInput, collection.classification.label);
+		await user.selectOptions(formElements.mediumInput, collection.medium.label);
 		await user.type(formElements.sizeWidthInput, collection.size.width.toString());
 		await user.type(formElements.sizeHeightInput, collection.size.height.toString());
 		await user.click(formElements.physicalTypeInput);
@@ -62,8 +62,8 @@ describe("CollectionForm", () => {
 		expect(formElements.titleInput).toHaveValue(collection.title);
 		expect(formElements.descriptionInput).toHaveValue(collection.description);
 		expect(formElements.artistInput).toHaveValue(collection.artist);
-		expect(formElements.classificationInput).toHaveTextContent(collection.classification);
-		expect(formElements.mediumInput).toHaveTextContent(collection.medium);
+		expect(formElements.classificationInput).toHaveValue(collection.classification.value);
+		expect(formElements.mediumInput).toHaveValue(collection.medium.value);
 		expect(formElements.sizeWidthInput).toHaveValue(collection.size.width);
 		expect(formElements.sizeHeightInput).toHaveValue(collection.size.height);
 		expect(formElements.digitalTypeInput).not.toBeChecked();
